15b303ddb2c1c9a9fbb7c3e09cdf47ceed60d201
Manikanta Chitimereddi <chitimereddimanikanta2006@gmail.com>
2026-01-26 20:44:38 +0530

    add author route to user api

--- server/APIs/userApi.js
+++ server/APIs/userApi.js
@@ -1,27 +1,26 @@
 const express = require("express");
 const userApp = express.Router();
-const userAuthor = require("../models/userAuthorModel");
 const expressAsyncHandler = require("express-async-handler");
 const createUserOrAuthor = require("./createUserOrAuthor");
 const Article = require("../models/articleModel");
 
-//API
+// create user
+userApp.post("/user", expressAsyncHandler(createUserOrAuthor));
 
+// 🔥 ADD THIS LINE
+userApp.post("/author", expressAsyncHandler(createUserOrAuthor));
 
-//create new user
-userApp.post("/user",expressAsyncHandler(createUserOrAuthor));
-
-//add comment to an article
-userApp.put("/comment/:articleId",expressAsyncHandler(async(req,res)=>{
+// add comment
+userApp.put("/comment/:articleId", expressAsyncHandler(async(req,res)=>{
 
     const commentObj = req.body;
     const articleWithComments = await Article.findOneAndUpdate(
         {articleId : req.params.articleId},
         {$push: {comments : commentObj}},
-        {returnOriginal : false});
-        res.status(200).send({ message: "comment added", payload: articleWithComments });
+        {returnOriginal : false}
+    );
 
-
+    res.status(200).send({ message: "comment added", payload: articleWithComments });
 }));
 
 userApp.delete(
@@ -29,7 +28,6 @@ userApp.delete(
   expressAsyncHandler(async (req, res) => {
     const { articleId, commentId } = req.params;
 
-    // remove comment by _id
     const updatedArticle = await Article.findOneAndUpdate(
       { articleId },
       { $pull: { comments: { _id: commentId } } },
@@ -39,8 +37,5 @@ userApp.delete(
     res.status(200).send({ message: "comment deleted", payload: updatedArticle });
   })
 );
-
-
-
 
 module.exports = userApp;
